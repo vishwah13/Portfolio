1185b3ce123b0729e22f2e7c1b691e3c2eea23b7
vishwahrocks <c1046213@newcastle.ac.uk>
2023-09-15 12:05:09 +0100

    domain name updated

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -8,7 +8,7 @@
     <ul>
       <li>
         <i class="fa fa-user-circle-o fa-lg fa-fw"></i>
-        <a href="http://mywebsite.com" target="_blank">https://mywebsite.com</a>
+        <a href="http://vishwah.com" target="_blank">https://vishwah.com</a>
       </li>
       <li>
         <i class="fa fa-envelope-o fa-lg fa-fw"></i>
