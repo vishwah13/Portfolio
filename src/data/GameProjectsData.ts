9ece4e3df4ec574bd2676a1b3e5488034a14290b
vishwahrocks <c1046213@newcastle.ac.uk>
2023-12-15 12:11:34 +0000

    added new project and Updated my resume

--- src/data/GameProjectsData.ts
+++ src/data/GameProjectsData.ts
@@ -4,7 +4,7 @@ export default [
     new ProjectData("project-1", "Flatland In Chaos", "img/projects/FlatLand1.webp",
     `
     <div class="paragraph">
-     <strong>Flatland In Chaos</strong> is a thing of beauty that I am so proud of, because I learn to complete game from scratch and published it on Google play store. Built using <strong>unity and C# </strong>
+     <strong>Flatland In Chaos</strong> is a thing of beauty that I am so proud of, because I learn to complete game from scratch and published it on Google play store. Built using <strong>Unity and C# </strong>
      
     </div>
     <div class="paragraph center">
@@ -31,7 +31,36 @@ export default [
         <img class="phone-screenshot" src="img/projects/FlatLand4.webp" alt="Flatland in Chaos Screenshot" />
     </div>
     `, "#23bd69", true),
-    new ProjectData("project-5", "StuBrew VR in UE5", "img/projects/StuBrew2.png", `
+    new ProjectData("project-2", "T-Wrecks", "img/projects/T-wrecks.gif", `
+    <div class="paragraph">
+    <strong>T-wrecks</strong> is a 2D Action Platformer game, where the player needs to use the ball to defend him self form enemies, made for Score Space Jam #14, made using <strong>Unity and C#</strong>  .
+    </div>
+    <div class="paragraph center">
+        <img class="pc-screenshot" src="img/projects/T-wrecks.gif" alt="T-wrecks Gif" />
+    </div>
+    <div class="paragraph">
+        Main features :
+        <ul>
+        <li>Bouncing ball physics</li>
+        <li>High score system</li>
+        <li>Game feel and effects</li>
+        <li>Endless gameplay</li>
+        <li>Object Oriented programming</li>
+        </ul>
+    </div>
+
+    <div class="paragraph">
+        <div class="notice">
+        Play on browser and Windows is available at <a href="https://vishwah13.itch.io/t-wrecks" target="_blank">itch.io</a>.
+        </div>
+    </div>
+
+    <div class="paragraph center">
+        <img class="pc-screenshot" src="img/projects/T-wrecks.png" alt="StuBrew VR Screenshot" />
+        <img class="pc-screenshot" src="img/projects/T-wrecks2.png" alt="StuBrew VR Screenshot" />
+    </div>
+    `, "#e80fb7"),
+    new ProjectData("project-3", "StuBrew VR in UE5", "img/projects/StuBrew2.png", `
     <div class="paragraph">
     <strong>StuBrew VR</strong> is a Virtual Reality game made for my dissertation, It is a interactive beer brewing experience made using <strong>Unreal Engine 5</strong>  .
     </div>
@@ -61,7 +90,7 @@ export default [
         <img class="pc-screenshot" src="img/projects/StuBrew3.png" alt="StuBrew VR Screenshot" />
     </div>
     `, "#e80fb7"),
-    new ProjectData("project-3", "UE5 Melee Combat System", "img/projects/UE5_MCS.gif", `
+    new ProjectData("project-4", "UE5 Melee Combat System", "img/projects/UE5_MCS.gif", `
     <div class="paragraph">
                 <strong>Melee Combat System</strong> is a gameplay system build inside <strong>Unreal Engine 5</strong> using <strong>Gameplay Ability System</strong> and <strong>C++</strong>, inspired by dark souls.
             </div>
@@ -88,7 +117,7 @@ export default [
                 <img class="pc-screenshot" src="img/projects/UE5_MCS2.gif" alt="Drew Blood Screenshot" />
                 <img class="pc-screenshot" src="img/projects/UE5_MCS3.gif" alt="Drew Blood Screenshot" /> 
             </div>`, "#1ca1e2"),
-    new ProjectData("project-2", "Hopin", "img/projects/Hopin.gif", `
+    new ProjectData("project-5", "Hopin", "img/projects/Hopin.gif", `
     <div class="paragraph">
         <strong>Hopin</strong> is a platformer/puzzle game, Made for ludum dare 49. Built using <strong>unity and C# </strong>
         <br/>Art by <a target="_blank" href="https://kaylousberg.itch.io/">KAYKIT</a>.
@@ -120,7 +149,7 @@ export default [
     <img class="pc-screenshot" src="img/projects/Hopin5.png" alt="Hopin Screenshot" />
     </div>
     `, "#5a78af"),
-    new ProjectData("project-9", "Vulkan graphics engine", "img/projects/Vulkan_teapot.gif", `
+    new ProjectData("project-6", "Vulkan graphics engine", "img/projects/Vulkan_teapot.gif", `
     <div class="paragraph">
                 A <strong>graphics engine </strong> built using Vulkan and C++.
             </div>
@@ -147,7 +176,7 @@ export default [
                 <img class="pc-screenshot" src="img/projects/Vulkan_Cube.gif" alt="Vulkan Screenshot" />
                 <img class="pc-screenshot" src="img/projects/Vulkan_Triangle.gif" alt="Vulkan Screenshot" /> 
             </div>`, "#1ca1e2"),
-    new ProjectData("project-4", "In Your Nightmare", "img/projects/IYM.png", `
+    new ProjectData("project-7", "In Your Nightmare", "img/projects/IYM.png", `
     <div class="paragraph">
         <strong>In Your Nightmare</strong> is my first <strong>procedural generation</strong> project, where each time new maze is created for the player to escape and also an AI chases the player. Built using <strong>unity and C# </strong>
     </div>
@@ -176,7 +205,7 @@ export default [
         <img class="pc-screenshot" src="img/projects/IYM3.png" alt="In Your Nightmare Screenshot" />
     </div>
     `, "#383838"),
-    new ProjectData("project-6", "PaintGame in OpenGL", "img/projects/splats.png", `
+    new ProjectData("project-8", "PaintGame in OpenGL", "img/projects/splats.png", `
     <div class="paragraph">
         <strong>PaintGame</strong> is a team project, It is a spalatoon-like game where the player have to vandalise a museum by spraying paint on the paintings and the statues, 
         And a NPC character tries to clean the paint and catch the player.
@@ -219,7 +248,7 @@ export default [
         <img class="pc-screenshot" src="img/projects/splats3.png" alt="splats Screenshot" />
         <img class="pc-screenshot" src="img/projects/splats4.png" alt="splats Screenshot" />
     </div>`, "#f70207"),
-    new ProjectData("project-7", "Doodle Jump in C", "img/projects/Doodle.png", `
+    new ProjectData("project-9", "Doodle Jump in C", "img/projects/Doodle.png", `
     <div class="paragraph">
         <strong>Doodle Jump in C</strong> is a clone of Doodle jump built using Raylib library.
     </div>
@@ -244,7 +273,7 @@ export default [
         <img class="pc-screenshot" src="img/projects/Doodle.png" alt="Doodle Screenshot" />
         <img class="pc-screenshot" src="img/projects/Doodle2.png" alt="Doodle Screenshot" />
     </div>`, "#383838"),
-    new ProjectData("project-8", "Ultimate BreakOut", "img/projects/BreakOut.gif", `
+    new ProjectData("project-10", "Ultimate BreakOut", "img/projects/BreakOut.gif", `
     <div class="paragraph">
         <strong>Ultimate BreakOut</strong> is a clone of Breakout built using Godot game engine for GMTK 2021 game jam.
     </div>
@@ -268,7 +297,7 @@ export default [
         <img class="pc-screenshot" src="img/projects/BreakOut.gif" alt="BreakOut Screenshot" />
         <img class="pc-screenshot" src="img/projects/BreakOut2.jpg" alt="BreakOut Screenshot" />
     </div>`, "#23bd69"),
-                    new ProjectData("project-10", "UE4 Platformer", "img/projects/Platformer_2.png", `
+                    new ProjectData("project-11", "UE4 Platformer", "img/projects/Platformer_2.png", `
                     <div class="paragraph">
                                 A <strong> story based third person platforming </strong> game built using <strong>unreal Engine 4.27</strong>.
                             </div>
